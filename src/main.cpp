f8662a310ae372bebae1cc61abefd9c36a7c9939
wei <coolbreeze23@163.com>
2025-03-25 19:49:47 +0800

    修复编译报错

--- src/main.cpp
+++ src/main.cpp
@@ -7,50 +7,50 @@ int APin = 13;
 Servo servo;
 Servo servo2;
 int freq = 1000;
-const double start_delay = 12.0;
-std::vector<double> deltas;
-std::vector<float> deltas2;
-std::vector<float> deltas3;
-void delayfunc()
+const double start_delay = 3.0;
+const float end_delay = 12.0;
+const float height_min = 5;
+const float height_max = 30;
+const int height_step = 20;
+const float weight_max = 95;
+const float weight_min = 50;
+const int weight_stepnum = 50;
+const int delay_stepnum = weight_stepnum + height_step;
+
+std::vector<float> delay_vector;
+std::vector<float> height_vector;
+std::vector<float> weight_vector;
+
+void delayfunc(std::vector<float> &deltas,float start = 3, float end = 12, int stepnum = 70)
 {
-  const double end = 3;
-  const int steps = 50;
+  float b = start;
+  float a = (end - b) / pow(stepnum, 2);
 
-  // 计算二次函数系数k
-  double sum_squares = 0;
-  for (int i = 0; i < steps; ++i)
+  for (int i = 0; i < stepnum; i += 1)
   {
-    sum_squares += pow(i, 2);
-  }
-  const double k = (start_delay - end) / sum_squares;
-
-  // 生成步长序列
-
-  for (int i = 0; i < steps; ++i)
-  {
-    deltas.push_back(k * pow(i, 2));
-  }
-}
-
-void heightFunc(float start=4,float end=30,int stepnum=20,std::vector<float> &deltas)
-{
-  //y = 0.065*x*x+4
-  float b=4;
-  float a=(end-b)/pow(stepnum,2);
-
-  for(int i=0;i<stepnum;i+=1)
-  {
-    deltas.push_back(a*pow(i, 2)+b);
+    deltas.push_back(a * pow(i, 2) + b);
   }
 }
 
-void weightFunc(float start=50,float end=95,int stepnum=50,std::vector<float> &deltas)
+void heightFunc(std::vector<float> &deltas,float start = 4, float end = 30, int stepnum = 20)
 {
-  float b=50;
-  float a=(end-b)/pow(stepnum,3);
-  for(int i=0;i<stepnum;i+=1)
+  // y = 0.065*x*x+4
+  float b = start;
+  float a = (end - b) / pow(stepnum, 2);
+
+  for (int i = 0; i < stepnum; i += 1)
   {
-    deltas.push_back(a*pow(i, 3)+b);
+    deltas.push_back(a * pow(i, 2) + b);
+  }
+}
+
+void weightFunc(std::vector<float> &deltas,float start = 50, float end = 95, int stepnum = 50)
+{
+  float b = start;
+  float a = (end - b) / pow(stepnum, 3);
+  for (int i = 0; i < stepnum; i += 1)
+  {
+    deltas.push_back(a * pow(i, 3) + b);
   }
 }
 
@@ -66,12 +66,13 @@ void setup()
   servo.attach(APin);
   servo2.attach(12);
 
-  delayfunc();
+  delayfunc(delay_vector,start_delay, end_delay, delay_stepnum);
+  heightFunc(height_vector,height_min, height_max, height_step);
+  weightFunc(weight_vector,weight_min, weight_max, weight_stepnum);
   delay(1000);
 }
 
 double height = 0;
-double height_min = 5;
 double orig = 1.0;
 double coe = 0.5;
 double weight_start = 50;
@@ -88,9 +89,9 @@ void loop()
   delayTime = start_delay;
   servo2.write(weight_start);
 
-  for (; height > height_min; height-=2, nums++)
+  for (; height > height_min; height -= 2, nums++)
   {
-    delayTime -= deltas[nums];
+    delayTime -= delay_vector[nums];
     delay(delayTime);
     servo.write(height);
   }
@@ -101,19 +102,17 @@ void loop()
     height = height + (orig + sin(height * (M_PI / 180.0))) * coe;
     servo.write(height);
     servo2.write(i);
-    delayTime -= deltas[nums];
-    //Serial.println(nums);
+    delayTime -= delay_vector[nums];
+    // Serial.println(nums);
     delay(delayTime);
   }
   Serial.println("2");
-  for (; height < 30; height+=2, nums++)
+  for (; height < 30; height += 2, nums++)
   {
     servo.write(height);
-    delayTime -= deltas[nums];
+    delayTime -= delay_vector[nums];
     delay(delayTime);
   }
   Serial.println("3");
   delay(200);
-  
-  
 }
