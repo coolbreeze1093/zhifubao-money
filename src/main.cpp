0c09692ddabeb7d332164d47d8ad75d688840999
wei <coolbreeze23@163.com>
2025-04-09 22:39:42 +0800

    新参数

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,26 @@
 #include <vector>
 
 int APin = 13;
-Servo servo;
-Servo servo2;
+Servo servo_h;
+Servo servo_w;
 int freq = 1000;
-const double start_delay = 3.0;
+const double start_delay = 2.5;
 const float end_delay = 12.0;
-const float height_min = 5;
-const float height_max = 30;
-const int height_step = 20;
-const float weight_max = 95;
-const float weight_min = 50;
-const int weight_stepnum = 50;
-const int delay_stepnum = weight_stepnum + height_step;
+const float height_start = 7;
+const float height_min = 3;
+const float height_max = 80;
+const float weight_max = 80;
+const float weight_min = 45;
+const int step = 50;
 
 std::vector<float> delay_vector;
 std::vector<float> height_vector;
 std::vector<float> weight_vector;
 
-void delayfunc(std::vector<float> &deltas,float start = 3, float end = 12, int stepnum = 70)
+void delayfunc(std::vector<float> &deltas,float start = 5, float end = 12, int stepnum = 70)
 {
-  float b = start;
-  float a = (end - b) / pow(stepnum, 2);
+  float b = end;
+  float a = (start - b) / pow(stepnum, 2);
 
   for (int i = 0; i < stepnum; i += 1)
   {
@@ -32,25 +31,40 @@ void delayfunc(std::vector<float> &deltas,float start = 3, float end = 12, int s
   }
 }
 
-void heightFunc(std::vector<float> &deltas,float start = 4, float end = 30, int stepnum = 20)
+void heightFunc(std::vector<float> &deltas,float start = 13, float end = 30,float min=4, int stepnum = 20)
 {
   // y = 0.065*x*x+4
-  float b = start;
-  float a = (end - b) / pow(stepnum, 2);
+  double b = min;
+  double c = (double)stepnum/4.0;
+  double a = (start - b) / pow(c, 2);
+
+  Serial.print("heightFunc: a:");
+  Serial.print(a);
+  Serial.print("  b:");
+  Serial.print(b);
+  Serial.print("  c:");
+  Serial.println(c);
+
 
   for (int i = 0; i < stepnum; i += 1)
   {
-    deltas.push_back(a * pow(i, 2) + b);
+    double _r=a * pow((i-c), 2) + b;
+    deltas.push_back(_r);
+    Serial.print("heightFunc: ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(deltas[i]);
   }
 }
 
 void weightFunc(std::vector<float> &deltas,float start = 50, float end = 95, int stepnum = 50)
 {
-  float b = start;
-  float a = (end - b) / pow(stepnum, 3);
+  float b = end;
+  float c = stepnum;
+  float a = (start - b) / pow(c, 2);
   for (int i = 0; i < stepnum; i += 1)
   {
-    deltas.push_back(a * pow(i, 3) + b);
+    deltas.push_back(a * pow((i-c), 2) + b);
   }
 }
 
@@ -63,56 +77,48 @@ void setup()
   ESP32PWM::allocateTimer(3);
   Serial.begin(115200);
 
-  servo.attach(APin);
-  servo2.attach(12);
+  servo_h.attach(APin);
+  servo_w.attach(12);
 
-  delayfunc(delay_vector,start_delay, end_delay, delay_stepnum);
-  heightFunc(height_vector,height_min, height_max, height_step);
-  weightFunc(weight_vector,weight_min, weight_max, weight_stepnum);
+  delayfunc(delay_vector,start_delay, end_delay, step);
+  heightFunc(height_vector,height_start, height_max,height_min, step);
+  weightFunc(weight_vector,weight_min, weight_max, step);
   delay(1000);
 }
-
-double height = 0;
-double orig = 1.0;
-double coe = 0.5;
-double weight_start = 50;
-double delayTime = start_delay;
-double width = weight_start;
 void loop()
 {
-  int rand_val = random(4, 8);
+  int rand_val = random(1, 10);
   delay(rand_val * 1000);
 
-  Serial.println(rand_val);
+  //Serial.println(rand_val);
 
-  int nums = 0;
-  delayTime = start_delay;
-  servo2.write(weight_start);
+  for(int i = weight_vector[step-1]; i > weight_vector[0]; i-=1)
+  {
+     servo_w.write(i);
+     delay(3);
+  }
+  delay(500);
+  for(int i=height_vector[step-1];i>height_vector[0];i-=1)
+  {
+    servo_h.write(i);
+    delay(3);
+  }
+  
+  for (int i = 0; i < step; i++)
+  {
+    for(int j=0;j<10;j++)
+    {
 
-  for (; height > height_min; height -= 2, nums++)
-  {
-    delayTime -= delay_vector[nums];
-    delay(delayTime);
-    servo.write(height);
+    }
+    servo_w.write(weight_vector[i]);
+    servo_h.write(height_vector[i]);
+    delay(delay_vector[i]);
+    //Serial.print("delay:  ");
+    //Serial.println(delay_vector[i]);
+    Serial.print("height: ");
+    Serial.println(height_vector[i]);
+    Serial.print("weight: ");
+    Serial.println(weight_vector[i]);
+    //Serial.println("***********");
   }
-  Serial.println("1");
-  height = height_min;
-  for (int i = weight_start; i < 95; i += 2, nums++)
-  {
-    height = height + (orig + sin(height * (M_PI / 180.0))) * coe;
-    servo.write(height);
-    servo2.write(i);
-    delayTime -= delay_vector[nums];
-    // Serial.println(nums);
-    delay(delayTime);
-  }
-  Serial.println("2");
-  for (; height < 30; height += 2, nums++)
-  {
-    servo.write(height);
-    delayTime -= delay_vector[nums];
-    delay(delayTime);
-  }
-  Serial.println("3");
-  delay(200);
 }
